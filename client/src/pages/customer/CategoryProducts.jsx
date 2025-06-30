f12fb691a05a7e0bff2407ee42b1821157a32645
Dinesh0007000 <164600789+Dinesh0007000@users.noreply.github.com>
2025-06-30 09:06:42 +0530

    Update CategoryProducts.jsx

--- client/src/pages/customer/CategoryProducts.jsx
+++ client/src/pages/customer/CategoryProducts.jsx
@@ -3,61 +3,61 @@ import Footer from '../../components/Footer'
 import Restaurants from '../../components/Restaurants'
 import '../../styles/CategoryProducts.css'
 import { useNavigate, useParams } from 'react-router-dom'
-import axios from 'axios'
+import axiosInstance from '../../components/AxiosInstance';
 
 const CategoryProducts = () => {
 
   const navigate = useNavigate();
 
-  const {category} = useParams();
+  const { category } = useParams();
 
   const [restaurants, setRestaurants] = useState([]);
 
-    useEffect(()=>{
-        fetchRestaurants();
-      }, [])
+  useEffect(() => {
+    fetchRestaurants();
+  }, [])
 
-    const fetchRestaurants = async() =>{
-        await axios.get('http://localhost:6001/fetch-restaurants').then(
-          (response)=>{
-            setRestaurants(response.data.filter(restaurant=> restaurant.menu.includes(category)));
-          }
-        )
+  const fetchRestaurants = async () => {
+    await axiosInstance.get('/fetch-restaurants').then(
+      (response) => {
+        setRestaurants(response.data.filter(restaurant => restaurant.menu.includes(category)));
       }
+    )
+  }
 
 
   return (
     <div className="categoryProducts-page">
 
-    <h2>Restaurants Serving {category}</h2>
+      <h2>Restaurants Serving {category}</h2>
 
-    
-    <div className="restaurants-container">
+
+      <div className="restaurants-container">
         <div className="restaurants-body">
-            <h3></h3>
-            <div className="restaurants">
+          <h3></h3>
+          <div className="restaurants">
 
-                {restaurants.map((restaurant) =>(
+            {restaurants.map((restaurant) => (
 
-                  <div className='restaurant-item' key={restaurant._id}>
-                      <div className="restaurant" onClick={()=> navigate(`/restaurant/${restaurant._id}`)}>
-                          <img src={restaurant.mainImg} alt="" />
-                          <div className="restaurant-data">
-                              <h6>{restaurant.title}</h6>
-                              <p>{restaurant.address}</p>
-                          </div>
-                      </div>
+              <div className='restaurant-item' key={restaurant._id}>
+                <div className="restaurant" onClick={() => navigate(`/restaurant/${restaurant._id}`)}>
+                  <img src={restaurant.mainImg} alt="" />
+                  <div className="restaurant-data">
+                    <h6>{restaurant.title}</h6>
+                    <p>{restaurant.address}</p>
                   </div>
-                ))}
+                </div>
+              </div>
+            ))}
 
- 
-            </div>
+
+          </div>
         </div>
-    </div>
+      </div>
 
 
 
-    <Footer />
+      <Footer />
     </div>
   )
 }
